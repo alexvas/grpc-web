af9240e1c1c4eb19076be4a70ecc5811c8824e24
Alexander Vasiljev <aavasiljev@ac-sw.com>
2019-02-12 13:30:51 +0700

    fix final artefact copy
    Use gConnector_static.zip instead of gConnector.zip

--- scripts/ubuntu_16_04.sh
+++ scripts/ubuntu_16_04.sh
@@ -20,5 +20,5 @@ cd ..
 make clean
 docker build -t ubuntu_16_04 -f net/grpc/gateway/docker/ubuntu_16_04/Dockerfile .
 CONTAINER_ID=$(docker create ubuntu_16_04)
-docker cp "$CONTAINER_ID:/github/grpc-web/gConnector.zip" net/grpc/gateway/docker/ubuntu_16_04
+docker cp "$CONTAINER_ID:/github/grpc-web/gConnector_static.zip" net/grpc/gateway/docker/ubuntu_16_04
 docker rm "$CONTAINER_ID"
